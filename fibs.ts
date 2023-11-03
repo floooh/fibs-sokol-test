74a7f99357daf1e8c2fdb626afca8d88ebcd46ea
Andre Weissflog <floooh@gmail.com>
2023-11-03 14:31:16 +0100

    bump fibs to v1.6.0

--- fibs.ts
+++ fibs.ts
@@ -1,5 +1,5 @@
-import * as fibs from 'https://deno.land/x/fibs@v1.5.0/mod.ts'
-//import * as fibs from "../fibs/mod.ts";
+//import * as fibs from 'https://deno.land/x/fibs@v1.6.0/mod.ts'
+import * as fibs from "../fibs/mod.ts";
 
 if (import.meta.main) fibs.main();
 
