3aa5386a43101fa5e6dd37bb51831bccc0591a06
Andre Weissflog <floooh@gmail.com>
2025-12-23 14:20:06 +0100

    fibs.ts code cleanup

--- fibs.ts
+++ fibs.ts
@@ -1,57 +1,30 @@
-import { Builder, Configurer, Platform, main } from "https://raw.githubusercontent.com/floooh/fibs/master/index.ts";
-
+import { Builder, Configurer, main, Platform } from 'https://raw.githubusercontent.com/floooh/fibs/master/index.ts';
 main(import.meta);
 
 export function configure(c: Configurer) {
-  c.addImport({
-    name: "libs",
-    url: "https://github.com/floooh/fibs-libs",
-    files: ["sokol.ts", "stb.ts"],
-  });
-  c.addImport({
-    name: "platforms",
-    url: "https://github.com/floooh/fibs-platforms",
-    files: ["emscripten.ts"],
-  });
-  c.addImport({
-    name: "utils",
-    url: "https://github.com/floooh/fibs-utils",
-    files: ["stdoptions.ts", "copyfiles.ts", "sokolshdc.ts"],
-  });
+  c.addImport({ name: 'libs', url: 'https://github.com/floooh/fibs-libs', files: ['sokol.ts', 'stb.ts'] });
+  c.addImport({ name: 'platforms', url: 'https://github.com/floooh/fibs-platforms', files: ['emscripten.ts'] });
+  c.addImport({ name: 'utils', url: 'https://github.com/floooh/fibs-utils', files: ['stdoptions.ts', 'copyfiles.ts', 'sokolshdc.ts'] });
 }
 
 export function build(b: Builder) {
-  // simple demo
-  b.addTarget("simple", "windowed-exe", (t) => {
-    t.setDir("src");
-    t.addSource("simple.c");
-    t.addDependencies(["sokol"]);
+  b.addTarget({ name: 'simple', type: 'windowed-exe', sources: ['src/simple.c'], deps: ['sokol'] });
+  b.addTarget('demo', 'windowed-exe', (t) => {
+    const shdcOutDir = b.targetBuildDir(t.name());
+    t.setDir('src');
+    t.addSources(['demo.c', 'demo.glsl']);
+    t.addDependencies(['sokol', 'stb', 'fileutil']);
+    t.addJob({ job: 'copyfiles', args: { srcDir: `${b.projectDir()}/assets`, files: ['baboon.png'] } });
+    t.addJob({ job: 'sokolshdc', args: { src: 'demo.glsl', outDir: shdcOutDir, slang: getSlang(b.platform()) }});
+    t.addIncludeDirectories([shdcOutDir]);
   });
-
-  // complex demo with assets and shaders
-  b.addTarget("demo", "windowed-exe", (t) => {
-    t.setDir("src");
-    t.addSources(["demo.c", "demo.glsl"]);
-    t.addDependencies(["sokol", "stb", "fileutil"]);
-    t.addIncludeDirectories(["@targetbuild:"]); // needed for sokol-shdc output
-    t.addJob({
-      job: "copyfiles",
-      args: { srcDir: "@root:assets", files: ["baboon.png"] },
-    });
-    t.addJob({
-      job: "sokolshdc",
-      args: { src: "demo.glsl", slang: getSlang(b.platform())}
-    });
-  });
-
-  // a helper library for file access
-  b.addTarget("fileutil", "lib", (t) => {
-    t.setDir("src");
-    t.addSource("fileutil.h");
+  b.addTarget('fileutil', 'lib', (t) => {
+    t.setDir('src');
+    t.addSource('fileutil.h');
     if (b.isMacOS()) {
-      t.addSource("fileutil_osx.m");
+      t.addSource('fileutil_osx.m');
     } else {
-      t.addSource("fileutil.c");
+      t.addSource('fileutil.c');
     }
   });
 }
